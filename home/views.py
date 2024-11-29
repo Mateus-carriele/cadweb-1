b12ef72e229b98bab933116bbf076e73cee406ca
Mateus-carriele <160273858+Mateus-carriele@users.noreply.github.com>
2024-11-28 21:02:10 -0300

    s

--- home/views.py
+++ home/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
-
 from .models import Categoria
 
 def index(request):
-    return render(request,'index.html')
+    return render(request, 'index.html')
 
 def categoria(request):
+    # Exibe a lista de categorias
     contexto = {
         'lista': Categoria.objects.all().order_by('id'),
     }
-    return render(request, 'categoria/lista.html' ,contexto)
+    return render(request, 'categoria/lista.html', contexto)
+
+def lista_produtos(request):
+    # Exibe a lista de produtos
+    lista = Categoria.objects.all()  # Corrigido 'objcts' para 'objects'
+    return render(request, 'categoria/lista.html', {'lista': lista})
