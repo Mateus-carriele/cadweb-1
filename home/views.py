4068f595d9d472a8694c85bcc1203874eec84c37
Mateus-carriele <160273858+Mateus-carriele@users.noreply.github.com>
2024-12-03 19:21:25 -0300

    ssss

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import Categoria
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import *
+from .forms import *
 
 def index(request):
     return render(request, 'index.html')
@@ -15,3 +16,21 @@ def lista_produtos(request):
     # Exibe a lista de produtos
     lista = Categoria.objects.all()  
     return render(request, 'categoria/lista.html', {'lista': lista})
+
+
+
+def form_categoria(request, id):
+    # Tenta encontrar o produto no banco de dados, se não encontrado, retorna 404
+    produto = get_object_or_404(Produto, id=id)
+
+    if request.method == 'POST':
+        # Preenche o formulário com os dados do POST
+        form = ProdutoForm(request.POST, instance=produto)
+        if form.is_valid():
+            form.save()  # Atualiza o produto no banco de dados
+            return redirect('produtos')  # Redireciona para a listagem de produtos
+    else:
+        # Preenche o formulário com os dados do produto para edição
+        form = ProdutoForm(instance=produto)
+
+    return render(request, 'produto/editar.html', {'form': form})
--- pweb/settings.py
+++ pweb/settings.py
@@ -79,10 +79,7 @@ WSGI_APPLICATION = 'pweb.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-   'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': dj_database_url.parse('postgres://neondb_owner:uOhozSGk6g9N@ep-flat-hall-a4moyzfz-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require')
 }
 
 
