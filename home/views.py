afb9bb5c123e4ef4a5eba0ef81b9f0ba20ab9f7e
Mateus-carriele <160273858+Mateus-carriele@users.noreply.github.com>
2024-12-18 10:44:30 -0300

    ss

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import *
 from .forms import *
+from django.contrib import messages
 
 def index(request):
     return render(request, 'index.html')
